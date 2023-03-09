f00d273452ab22a64f967afbe7fcedc1d4ebcc22
Niels Cnops <cnops_niels@hotmail.com>
2023-03-09 10:51:12 +0100

    added getById endpoint

--- php/230217/api.php
+++ php/230217/api.php
@@ -5,6 +5,16 @@ require "includes/Track.class.php";
 $args = $_REQUEST;
 $args['qsparts'] = explode('/', $args['qs']);
 
+
+
+
+
+
+
+
+
+
+
 $response = new StdClass;
 
 switch ($args['qsparts'][1]) {
@@ -16,7 +26,7 @@ switch ($args['qsparts'][1]) {
     $filters = [];
 
     if (isset($args['genre'])) {
-        $filters['genre'] = $args['genre'];
+      $filters['genre'] = $args['genre'];
     }
 
     if (isset($args['artist_name'])) {
@@ -27,27 +37,41 @@ switch ($args['qsparts'][1]) {
     $response->total_items = $track->getTotal($filters);
     $response->total_pages = ceil($response->total_items / $limit);
 
-    if (isset($args['page']) && 
-      (is_numeric($args['page'])) && 
-      ($args['page'] <= $response->total_pages) && 
-      ($args['page'] > 0)) {
-        $response->page = (int)$args['page'];
+    if (
+      isset($args['page']) &&
+      (is_numeric($args['page'])) &&
+      ($args['page'] <= $response->total_pages) &&
+      ($args['page'] > 0)
+    ) {
+      $response->page = (int)$args['page'];
     }
 
     $response->results = $track->getAll(($response->page - 1) * $limit, $limit, $filters);
 
     if ($response->page < $response->total_pages) {
       $filters['page'] = $response->page + 1;
-      $response->next_page_url= 'http://localhost/230217/api/v1/tracks?' . http_build_query($filters);
+      $response->next_page_url = 'http://localhost/230217/api/v1/tracks?' . http_build_query($filters);
     }
 
     if ($response->page > 1) {
       $filters['page'] = $response->page - 1;
-      $response->previous_page_url= 'http://localhost/230217/index.php?page=' . http_build_query($filters);
+      $response->previous_page_url = 'http://localhost/230217/index.php?page=' . http_build_query($filters);
     }
 
     break;
-  
+
+
+  case "track":
+    $db = new Db();
+    $track = new Track($db);
+    $limit = 50;
+
+
+    $id = $args["qsparts"][2];
+    $response = $track->getById($id);
+
+    break;
+
   default:
     $response->error = "This is not a valid endpoint.";
     break;
--- php/230217/includes/Track.class.php
+++ php/230217/includes/Track.class.php
@@ -1,30 +1,43 @@
 <?php
 
-Class Track {
+class Track
+{
 
   private $db;
-  
 
-  public function __construct($db) {
+
+  public function __construct($db)
+  {
     $this->db = $db;
   }
 
 
-  public function getTotal($filters = []) {
+  public function getTotal($filters = [])
+  {
     $where = $this->buildWhere($filters);
     $sql = "SELECT COUNT(id) AS total FROM 230217_tracks $where";
     return $this->db->executeQuery($sql, $filters)[0]->total;
   }
-  
 
-  public function getAll($offset = 0, $limit = 50, $filters = []) {
+
+  public function getAll($offset = 0, $limit = 50, $filters = [])
+  {
     $where = $this->buildWhere($filters);
     $sql = "SELECT id, track_id, track_name, artist_name, genre FROM 230217_tracks $where LIMIT $limit OFFSET $offset";
     return $this->db->executeQuery($sql, $filters);
   }
 
+  public function getById($id, $offset = 0, $limit = 50)
+  {
+    $filters = ["id" => $id];
+    $where = $this->buildWhere($filters);
+    $sql = "SELECT * FROM 230217_tracks $where LIMIT $limit OFFSET $offset";
+    return $this->db->executeQuery($sql, $filters);
+  }
 
-  private function buildWhere($filters) {
+
+  private function buildWhere($filters)
+  {
     $where = '';
     if (count($filters)) {
       $where = [];
@@ -35,5 +48,4 @@ Class Track {
     }
     return $where;
   }
-
 }
